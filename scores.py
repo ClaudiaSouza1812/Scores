2d26f5f04634e9dcad5877430e6f85a4ea423dfc
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2023-05-24 15:17:22 +0100

    Create scores.py

---
+++ scores.py
@@ -0,0 +1,29 @@
+def main():
+    score = get_scores()
+    average = sum(score) / len(score)
+    print(f"Average: {average:.2f}")
+
+
+def get_scores():
+
+    scores = []
+
+    n = - 1
+    while n <= 0 or n > 10:
+        try:
+            n = int(input("Enter how many scores there should be: "))
+        except ValueError:
+            print("Not an integer")
+
+    c = 0
+    while c != n:
+        try:
+            score = int(input("Score: "))
+            scores.append(score)
+            c += 1
+        except ValueError:
+            print("Not an integer")
+    return scores
+
+
+main()
